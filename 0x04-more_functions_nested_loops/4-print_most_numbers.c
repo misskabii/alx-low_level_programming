9b3ed6d176c2e983469c68316696b5482dd78546
misskabii <shazmakesh254@gmail.com>
2023-02-26 12:34:37 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,12 @@ void print_most_numbers(void)
 {
 int num;
 
-	for(num = 0; num <= 9; num ++)
+	for (num = 0; num <= 9; num++)
 	{
 
-	if(num != 2 && num != 4)
+	if (num != 2 && num != 4)
 		_putchar(num + '0');
 	}
 
 	_putchar('\n');
-}	
+}
