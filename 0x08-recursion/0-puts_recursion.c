c3fe0439f76f158a3959095683ea5840bca4e0aa
misskabii <shazmakesh254@gmail.com>
2023-03-08 17:19:54 +0300

    task 0 recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *_puts_recursion - prints a string followed by new line
+  *@s: string to print
+  */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
