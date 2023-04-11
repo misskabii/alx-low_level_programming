23edb79406f6d7b189026a4b7f910c67a977915d
misskabii <shazmakesh254@gmail.com>
2023-04-11 22:45:18 +0300

    CP

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
